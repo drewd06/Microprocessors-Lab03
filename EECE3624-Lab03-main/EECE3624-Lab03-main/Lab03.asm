34d7bcc5c9c18b54a6234db0a9443db57f678d5a
drewd06 <drewdimino06@outlook.com>
2026-09-08 16:47:08 -0500

    Completed Program for Lab03

--- EECE3624-Lab03-main/EECE3624-Lab03-main/Lab03.asm
+++ EECE3624-Lab03-main/EECE3624-Lab03-main/Lab03.asm
@@ -29,6 +29,10 @@
  * NOTE:  For testing, you can modify these after loading them
  * to make sure all of your branches work properly
  ***********************************************************************/
+.equ THRESHOLD	= 0x90	; Constant
+.def Sensor1	= R20	; R20 nickname
+.def Sensor2	= R21	; R21 nickname
+
 
 .org 0x0000 ; next instruction will be written to address 0x0000
             ; (the location of the reset vector)
@@ -44,3 +48,58 @@ main:       ; jump here on reset
 
     ;----------------------------------
     ; student-written code begins here    
+
+	LDI YH, high(0x0100)	; set High Y to the MSBs of address 0x0100
+	LDI YL, low(0x0100)		; set Low Y to the LSBs of address 0x0100
+
+	LD Sensor1, Y+	; load byte stored at the address stored in Y (0x0100) to R20. Post-increment.
+	LD Sensor2, Y	; load byte stored at the address stored in Y (0x0101) to R21.
+
+	LDI YL, 0x10	; set Y to address 0x0110
+
+	CPI Sensor1, THRESHOLD	; compare value R20, 0x90
+	BRSH s1GoE				; branch if R20 >= 0x90
+
+	LDI R17, 0x50	; store 0x50 to address 0x0110 if R20 < 0x90. Post-increment.
+	RJMP storeS1	; jump to Sensor1 storage
+
+s1GoE:
+	
+	LDI R17, 0x46	; store 0x46 to address 0x0110 (branched). Post-increment.	
+
+storeS1:
+
+	ST Y+, R17	; store prior comparison result
+
+	ST Y+, Sensor1	; store Sensor1 data to address 0x0111. Post-increment.
+
+	CPI Sensor2, THRESHOLD	; compare value R21, 0x90
+	BRLT s2L
+
+	LDI R17, 's'	; store 's' to address 0x0112 if R21 >= 0x90. Post-increment.	
+	RJMP sC			; jump to Sensor comparison
+
+s2L:
+	
+	LDI R17, 'i'	; store 'i' to address 0x0112 if R21 < 0x90. Post-increment.
+
+sC:
+	
+	ST Y+, R17	; store prior comparison result
+
+	CP Sensor1, Sensor2		; compare value stored in R20, R21
+	BRNE sNE				; branch if values in R20, R21 are not equal.
+
+	LDI R17, 108	; store 108 to address 0x0113 if R20, R21 are equal
+	RJMP end		; end program
+
+sNE:
+
+	LDI R17, 115	; store 115 to address 0x0113 if R20, R21 not equal
+	RJMP end		; end program
+
+end:
+	
+	ST Y, R17	; store prior comparison result
+
+	NOP	; It's over, Anakin
